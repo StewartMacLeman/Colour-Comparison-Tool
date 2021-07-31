e9adf9d627743cd214907d2a618c39f9470a293f
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-07-31 22:22:04 +0100

    Scroll arrows added.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// let arrowCont = document.querySelectorAll(".arrowContainer");
+// console.log(arrowCont);
+// let arrowStyles = window.getComputedStyle(arrowCont[0]);
+// console.log(arrowStyles);
+// let arrowHeightPx = arrowStyles.getPropertyValue("height");
+// console.log(arrowHeightPx);
+// let parsedNumber = parseFloat(arrowHeightPx);
+// console.log(typeof parsedNumber);
+// console.log(parsedNumber);
+// let doubleHeight = parsedNumber * 2;
+// let doubleHeightString = doubleHeight.toString();
+// console.log(doubleHeightString);
+//
+// let arrows = document.querySelectorAll(".arrowContainer .arrow");
+//
+// arrows[0].style.height = doubleHeightString + "px";
+// arrows[0].style.width = doubleHeightString + "px";
+// arrows[0].style.translateY = "-24.25px";
+// arrows[1].style.height = doubleHeightString + "px";
+// arrows[1].style.width = doubleHeightString + "px";
